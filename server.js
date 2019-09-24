94ad505451f21ece6696d77261e3c6618a048061
Glitch (hello-express) <none>
2019-09-24 21:15:44 +0000

    🏂🍣 Checkpoint ./server.js:6775565/14 ./views/editor.html:6775565/14216 ./views/register.html:6775565/82 ./public/editor.js:6775565/1528

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/editor.html');
+  response.sendFile(__dirname + '/views/register.html');
 });
 
 app.get('/editor', function(request, response) {
