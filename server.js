e692035fe0b9b1cf056de9e88eafd4173fcf3eee
Glitch (hello-express) <none>
2019-10-08 20:54:11 +0000

    🐷🚌 Checkpoint ./server.js:6775565/3030

--- server.js
+++ server.js
@@ -2,59 +2,56 @@
 // where your node app starts
 
 // init project
-const express = require('express');
+const express = require("express");
 const app = express();
-const server = require('http').createServer(app);
+const server = require("http").createServer(app);
 
 // socket.io
-const io = require('socket.io')(server);
+const io = require("socket.io")(server);
 
 // listen for requests :)
 const listener = server.listen(process.env.PORT || 3000, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {  
-  response.sendFile(__dirname + '/views/register.html');
+app.get("/", function(request, response) {
+  response.sendFile(__dirname + "/views/register.html");
 });
 
-app.get('/editor', function(request, response) {
-  response.sendFile(__dirname + '/views/editor.html');
+// Just a proof of concept
+app.get("/editor", function(request, response) {
+  response.sendFile(__dirname + "/views/editor.html");
 });
 
-app.get('/concert', function(request, response) {
-  response.sendFile(__dirname + '/views/concert.html');
+app.get("/concert", function(request, response) {
+  response.sendFile(__dirname + "/views/concert.html");
 });
-
 
 // DEBUG ROUTE
-app.get('/num_connections', function(request, response) {
-  response.send({numConnections: Object.keys(connections)});
-})
+app.get("/num_connections", function(request, response) {
+  response.send({ numConnections: Object.keys(connections) });
+});
 
 const connections = {};
 
-io.on('connection', function(socket){
-  
-  socket.on('added user', function(username) {
+io.on("connection", function(socket) {
+  socket.on("added user", function(username) {
     // lets not worry about unique usernames...
-    console.log(`added user:  ${username}`)
-    const guid = username + (+ new Date());
+    console.log(`added user:  ${username}`);
+    const guid = username + +new Date();
     connections[guid] = { guid };
     socket.send(connections[guid]);
-    
   });
-  
-  socket.on('livecode-enter', function(guid) {
-    console.log('livecode-enter + ', guid);
-  })
-  
-  socket.on('disconnect', function(){
+
+  socket.on("livecode-enter", function(guid) {
+    console.log("livecode-enter + ", guid);
+  });
+
+  socket.on("disconnect", function() {
     //
   });
-  
 });
