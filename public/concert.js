6d3a0baed3d50eb8085498aa805153424d325cdf
Glitch (hello-express) <none>
2019-10-14 22:34:30 +0000

    🏊🏼✊ Checkpoint ./public/concert.js:1799356/617:6775565/67 ./views/concert.html:1799356/358

--- public/concert.js
+++ public/concert.js
@@ -24,7 +24,7 @@ uniform vec3 u_camPos;
 void main() {
   vec2 st = -1. + 2. * gl_FragCoord.xy/u_resolution;
 
-  gl_FragColor = vec4(st.x, ((sin(u_camPos.x)+1.0) / 2.0), 0.5, ((sin(u_camPos.y)+1.0) / 2.0));
+  gl_FragColor = vec4(st.x, ((sin(u_camPos.x)+1.0) / 2.0), 0.5, 1.0);
 }`;
 
 init();
@@ -98,13 +98,10 @@ function render() {
   // update camera position
   var _camera = document.querySelector("a-camera");
   var pos = _camera.getAttribute("position");
-  uniforms.u_camPos = new THREE.Vector3(pos.x, pos.y, pos.z);
-  
-  console.trace();
+  uniforms.u_camPos.value = new THREE.Vector3(pos.x, pos.y, pos.z);
+  // if there is no .value here we get a strange error from three.js.min sayinf b is undefined :0
   
   renderer.render( scene, threeCam );
-  
-
 
 }
 
