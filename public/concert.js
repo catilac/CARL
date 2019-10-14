d38535fecd74095dc2b84e81c1ffd3f53f1505f1
Glitch (hello-express) <none>
2019-10-14 23:58:17 +0000

    📰🌴 Checkpoint ./public/concert.js:1799356/3612:6775565/71

--- public/concert.js
+++ public/concert.js
@@ -13,9 +13,7 @@ var caurl_id;
 var socket;
 
 var _fragmentShader = `      
-#ifdef GL_ES
-  precision mediump float;
-#endif
+
 
 uniform vec2 u_resolution;
 uniform vec2 u_mouse;
@@ -23,13 +21,97 @@ uniform float u_time;
 uniform vec3 u_camRot;
 uniform sampler2D u_feed;
 
-void main() {
-  vec2 st = -1. + 2. * gl_FragCoord.xy/u_resolution;
+// Define some constants
+const int steps = 128; // This is the maximum amount a ray can march.
+const float smallNumber = 0.001;
+const float maxDist = 10.; // This is the maximum distance a ray can travel.
+ 
+float scene(vec3 position){
+    // So this is different from the sphere equation above in that I am
+    // splitting the position into its three different positions
+    // and adding a 10th of a cos wave to the x position so it oscillates left 
+    // to right and a (positive) sin wave to the z position
+    // so it will go back and forth.
+    float sphere = length(
+        vec3(
+            position.x + cos(u_time)/10., 
+            position.y, 
+            position.z+ sin(u_time) +1.)
+        )-0.5;
+    
+    // This is different from the ground equation because the UV is only 
+    // between -1 and 1 we want more than 1/2pi of a wave per length of the 
+    // screen so we multiply the position by a factor of 10 inside the trig 
+    // functions. Since sin and cos oscillate between -1 and 1, that would be 
+    // the entire height of the screen so we divide by a factor of 10.
+    float ground = position.y + sin(position.x * 10.) / 10. 
+                              + cos(position.z * 10.) / 10. + 1.;
+    
+    // We want to return whichever one is closest to the ray, so we return the 
+    // minimum distance.
+    return min(sphere,ground);
+}
+vec4 trace (vec3 origin, vec3 direction){
+    
+    float dist = 0.;
+    float totalDistance = 0.;
+    vec3 positionOnRay = origin;
+    
+    for(int i = 0 ; i < steps; i++){
+        
+        dist = scene(positionOnRay);
+        
+        // Advance along the ray trajectory the amount that we know the ray
+        // can travel without going through an object.
+        positionOnRay += dist * direction;
+        
+        // Total distance is keeping track of how much the ray has traveled
+        // thus far.
+        totalDistance += dist;
+        
+        // If we hit an object or are close enough to an object,
+        if (dist < smallNumber){
+            // return the distance the ray had to travel normalized so be white
+            // at the front and black in the back.
+            return 1. - (vec4(totalDistance) / maxDist);
+ 
+        }
+        
+        if (totalDistance > maxDist){
+ 
+            return texture2D(u_feed, gl_FragCoord.xy/u_resolution); // Background color.
+        }
+    }
+    
+    return texture2D(u_feed, gl_FragCoord.xy/u_resolution);
+}
+ 
+// main is a reserved function that is going to be called first
+void main(void)
+{
+    vec2 normCoord = gl_FragCoord.xy/u_resolution;
+
+    vec2 uv = -1. + 2. * normCoord;
+    // Unfortunately our screens are not square so we must account for that.
+    uv.x *= (u_resolution.x / u_resolution.y);
+    
+    vec3 rayOrigin = vec3(uv, 0.);
+    vec3 camOrigin = vec3(0., 0., -1.);
+    vec3 direction = camOrigin + rayOrigin;
+
+    // This reserved variable is what we must set the final color to
+    gl_FragColor = trace(rayOrigin, direction);
+}
+
+//void main() {
+//  vec2 st = -1. + 2. * gl_FragCoord.xy/u_resolution;
 
   // gl_FragColor = vec4(texture2D(u_feed,gl_FragCoord.xy * u_resolution).r, ((sin(u_camRot.x)+1.0) / 2.0), 0.5, 1.0);
 
-  gl_FragColor = texture2D(u_feed, gl_FragCoord.xy/u_resolution).rrrr;
-}`;
+//  gl_FragColor = texture2D(u_feed, gl_FragCoord.xy/u_resolution).rrrr;
+//}
+
+`;
 
 init();
 animate();
