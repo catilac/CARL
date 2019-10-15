63038d3f849a8e8d00f8f5b0f9b023f7336a0cfd
Glitch (hello-express) <none>
2019-10-15 17:18:47 +0000

    🚌🛋 Checkpoint ./public/concert.js:1799356/2651

--- public/concert.js
+++ public/concert.js
@@ -27,9 +27,39 @@ const float smallNumber = 0.001;
 const float maxDist = 10.; // This is the maximum distance a ray can travel.
  
 
-vec3 rotate_vector( vec4 quat, vec3 vec )
+vec3 rotateQuat( vec4 quat, vec3 vec )
 {
 return vec + 2.0 * cross( cross( vec, quat.xyz ) + quat.w * vec, quat.xyz );
+}
+
+vec3 lookAt(vec2 uv, vec3 camOrigin, vec3 camTarget){
+    vec3 zAxis = normalize(camTarget - camOrigin);
+    vec3 up = vec3(0,1,0);
+    vec3 xAxis = normalize(cross(up, zAxis));
+    vec3 yAxis = normalize(cross(zAxis, xAxis));
+    
+    float fov = 2.;
+    
+    vec3 dir = (normalize(uv.x * xAxis + uv.y * yAxis + zAxis * fov));
+    
+    return dir;
+}
+
+mat4 rotationMatrix(vec3 axis, float angle) {
+    axis = normalize(axis);
+    float s = sin(angle);
+    float c = cos(angle);
+    float oc = 1.0 - c;
+    
+    return mat4(oc * axis.x * axis.x + c,           oc * axis.x * axis.y - axis.z * s,  oc * axis.z * axis.x + axis.y * s,  0.0,
+                oc * axis.x * axis.y + axis.z * s,  oc * axis.y * axis.y + c,           oc * axis.y * axis.z - axis.x * s,  0.0,
+                oc * axis.z * axis.x - axis.y * s,  oc * axis.y * axis.z + axis.x * s,  oc * axis.z * axis.z + c,           0.0,
+                0.0,                                0.0,                                0.0,                                1.0);
+}
+
+vec3 rotateEuler(vec3 v, vec3 axis, float angle) {
+	mat4 m = rotationMatrix(axis, angle);
+	return (m * vec4(v, 1.0)).xyz;
 }
 
 float scene(vec3 position){
@@ -53,8 +83,7 @@ float scene(vec3 position){
     float ground = position.y + sin(position.x * 10.) / 10. 
                               + cos(position.z * 10.) / 10. + 1.;
     
-    // We want to return whichever one is closest to the ray, so we return the 
-    // minimum distance.
+   
     return min(sphere,ground);
 }
 vec4 trace (vec3 origin, vec3 direction){
@@ -105,9 +134,21 @@ void main(void)
     vec3 camOrigin = vec3(0., 0., -1.);
 
 
-    vec3 dir = camOrigin + rayOrigin;
+    
+    
 
-    dir = rotate_vector(u_camRot, dir);
+    vec3 zAxis = normalize(camTarget - camOrigin);
+    vec3 up = vec3(0,1,0);
+    vec3 xAxis = normalize(cross(up, zAxis));
+    vec3 yAxis = normalize(cross(zAxis, xAxis));
+
+    // we need to apply rotate 3 times each with rotation on the relative object, 
+    // then we can get the lookat direction that we need. SO lets start with looking at forward
+
+    vec3 dirToLook = zAxis;
+    
+
+    vec3 dir = lookAt();
 
     // This reserved variable is what we must set the final color to
     gl_FragColor = trace(rayOrigin, dir);
